f99d9eeaef6995e185c38191bbd5f7e90c13f0f9
Dylan Swartz <mail@dylanswartz.com>
2011-09-07 09:16:18 -0400

    first commit'

---
+++ extensisToDspace.pl
@@ -0,0 +1,73 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+
+$/ = "\r";               #set the input recpord seperator 
+open (DATA, "<$ARGV[0]") # open file from 1st command line arg
+        or die ("Unable to open file");
+
+my $XML_FILE        = "dublin_core.xml";
+my $CONTENTS_FILE   = "contents";
+my $header          = <DATA>;
+my $imagePath       = "";
+my $imageName       = "";
+my $imageAuthor     = "";
+my $collectionTitle = "";
+
+while ( my $row = <DATA> ) {
+	# for every record, get the data, build an xml file,
+	# and append to contents file.
+
+	last unless $row =~ /\s/; # this doesn't do anything. Idk why.
+	chomp $row; # removes trailing whitespace		
+	my @cells = split /\t/, $row; # split into array
+
+	# get image path
+	$imagePath = getImagePath(\@cells);
+	
+	# get image file name (used as title too)
+	$imageName = getImageFileName(\@cells);
+
+      	# get current directory name (collection name)
+	$collectionTitle = getCurrentDirectoryName(\@cells);
+
+	# get author
+	$imageAuthor = getAuthor(\@cells);
+	
+	# create xml file
+
+	# append to contents file	
+
+}
+
+# createXMLFIle
+# This subprocedure creates a Dublin Core XML file describing an image
+# Input: imagePath, imageFileName, collectionName, author
+sub createXMLFile { 
+
+}
+
+sub getImagePath {
+ my @cells = @{$_[0]};
+ my $string;
+ $cells[74]=~s/:/\//g;
+ $string = $cells[74];
+ $string = substr $string,4;
+ return $string;
+}
+
+sub getImageFileName {
+	return $_[0][33];
+}
+
+sub getCurrentDirectoryName {
+	my @cells = @{$_[0]};
+	$cells[74]=~s/:/\//g;
+	my $string = $cells[74];
+	my @dirname = split '/', $string;
+	return $dirname[-2]; 
+}
+
+sub getAuthor {
+	return $_[0][4];
+}	
