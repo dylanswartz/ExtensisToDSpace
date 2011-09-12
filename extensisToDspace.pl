9bcd2e87d410737399ef216a3f52e00f46b51d0d
Dylan Swartz <mail@dylanswartz.com>
2011-09-12 10:37:22 -0400

    implemented dublin_core generator and contents file generator

--- extensisToDspace.pl
+++ extensisToDspace.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/perl
 use strict;
 use warnings;
+use File::Basename;
 
 $/ = "\r";               #set the input recpord seperator 
 open (DATA, "<$ARGV[0]") # open file from 1st command line arg
@@ -14,6 +15,7 @@ my $imageName       = "";
 my $imageAuthor     = "";
 my $collectionTitle = "";
 
+
 while ( my $row = <DATA> ) {
 	# for every record, get the data, build an xml file,
 	# and append to contents file.
@@ -24,7 +26,7 @@ while ( my $row = <DATA> ) {
 
 	# get image path
 	$imagePath = getImagePath(\@cells);
-	print $imagePath."\n";
+	
 	# get image file name (used as title too)
 	$imageName = getImageFileName(\@cells);
 	
@@ -37,9 +39,14 @@ while ( my $row = <DATA> ) {
 	# create xml file
 	createXMLFile($imagePath, $imageName, $collectionTitle, $imageAuthor);
 
-	# append to contents file	
+	# append to contents file
+	appendContentsFile(\@cells, "$imagePath"."$CONTENTS_FILE");
 
 }
+# write last line in contents file
+open CONTENTS,"+>>", "$imagePath"."$CONTENTS_FILE";
+print CONTENTS"license.txt\tbundle:LICENSE";
+close CONTENTS;
 
 # createXMLFile
 # This subprocedure creates a Dublin Core XML file describing an image
@@ -50,8 +57,33 @@ sub createXMLFile {
 	my $collectionTitle = $_[2];
 	my $imageAuthor     = $_[3];
 
-	#open FILE, ">", "$imagePath.$XML_FILE" or die $!
-	#print $imagePath.$XML_FILE;
+	open FILE, ">", "$imagePath"."$XML_FILE" or die $!;
+	#print "$imagePath"."$XML_FILE\n";
+	print FILE "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>\n";
+	print FILE "<dublin_core schema=\"dc\">\n";
+	print FILE "<dcvalue element=\"contributor\" qualifier=\"author\">".$imageAuthor."</dcvalue>\n";
+	print FILE "<dcvalue element=\"language\" qualifier=\"iso\">en</dcvalue>\n";
+	print FILE "<dcvalue element=\"description\" qualifier=\"provenance\" language=\"en\">description weee</dcvalue>";
+	print FILE "<dcvalue element=\"title\" qualifier=\"none\" language=\"en_US\">". $collectionTitle ."</dcvalue>\n";
+	print FILE "</dublin_core>";
+}
+
+#ContentsFile
+#Input: cells, filename
+sub appendContentsFile {
+	my @cells = @{$_[0]};
+	my $contents = $_[1];
+	my $string;
+	$cells[74]=~s/:/\//g;
+	$string = $cells[74];
+	$string = substr $string,4;
+	my($filename,$directories)=fileparse($string);
+
+	$filename =~ s/\000//g;
+	open (TEXT,"+>>$contents");
+	print TEXT "$filename\tbundle:ORIGINAL\n";
+	close(TEXT);
+
 }
 
 sub getImagePath {
@@ -60,7 +92,9 @@ sub getImagePath {
 	$cells[74]=~s/:/\//g;
 	$string = $cells[74];
 	$string = substr $string,4;
-	return $string;
+	my($filename,$directories)=fileparse($string);
+	$directories=~ s/\000//g;
+	return $directories;
 }
 
 sub getImageFileName {
